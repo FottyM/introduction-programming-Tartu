fbf767dea1513d7076e68300d5157740f7099b1a
FottyM <fotimutunda@gmail.com>
2015-10-30 03:19:58 +0200

    This is a fix for the check account

--- bank.py
+++ bank.py
@@ -1,6 +1,6 @@
 __author__ = 'Fotty'
 import random
-from pip._vendor.distlib.compat import raw_input
+#from pip._vendor.distlib.compat import raw_input
 
 '''
 1. Create your own account
@@ -27,15 +27,7 @@ def create_account():
     account = open('data.txt', 'a')
     first_name = raw_input("first name: ")
     last_name = raw_input("last name: ")
-    password = int(raw_input("password 6 digits: "))
-    try:
-        if len(password) != 6:
-            print("Password should be 6 digits: ")
-        else:
-            print("Good ")
-    except:
-        print("try again")
-
+    password = raw_input("password 4 digits: ")
     balance = 0
     account_number = to_string(str(random.randint(0,10000000000) % 10000000000))
     acc_details = first_name + ","+last_name+","+str(password)+","+str(balance)+","+str(account_number) + "\n"
@@ -54,12 +46,9 @@ def deposit_money():
         dets = (lines[index].split(","))
 
         if (dets[0] == first_name) and (dets[2] == password):
-            money_to_deposit = raw_input("Enter the amount of money to deposit ")
+            money_to_deposit = raw_input("Enter the amount of money to deposit: ")
             sum = str(float(dets[3]) + float(money_to_deposit))
-            #print(sum)
-
             dets[3] = str(sum)
-            print(dets[3], dets)
             bigIndex = index
             break
 
@@ -70,14 +59,14 @@ def deposit_money():
     account = open('data.txt', 'w+')
     for element in lines:
         account.write(element)
-    print(dets)
+    print("Your balance is now: ",dets[3])
     return "Money deposited: ", dets[3]
 
 
 
 def withdraw_money():
     account = open('data.txt', 'r+')
-    first_name = raw_input("What's your name please: ")
+    first_name = str(raw_input("What's your name please: "))
     password = str(raw_input("Password please: "))
     lines = account.readlines()
     bigIndex = -1
@@ -86,14 +75,16 @@ def withdraw_money():
         dets = (lines[index].split(","))
 
         if (dets[0] == first_name) and (dets[2] == password):
-            money_to_withdraw = raw_input("Enter the amount of money to deposit ")
-            sum = str(float(dets[3]) - float(money_to_withdraw))
-            #print(sum)
-
-            dets[3] = str(sum)
-            print(dets[3], dets)
-            bigIndex = index
-            break
+            money_to_withdraw = raw_input("How much do you want to withdraw: ")
+            if float(money_to_withdraw) > 0 and float(money_to_withdraw) <= float(dets[3]):
+                sum = str(float(dets[3]) - float(money_to_withdraw))
+                dets[3] = str(sum)
+                print(dets[3],"Withdrawn ")
+                bigIndex = index
+            else:
+                print("Not enough money into your account")
+        else:
+            print("Wrong credentials please try again: ")
 
     if(bigIndex != -1):
         lines[bigIndex] = (",".join(el for el in dets))
@@ -102,27 +93,26 @@ def withdraw_money():
     account = open('data.txt', 'w+')
     for element in lines:
         account.write(element)
-    print(dets)
-    return "Money deposited: ", dets[3]
+
+    return "Money deposited: " + dets[3]
 
 
 
 def check_account():
     account = open('data.txt', 'r+')
     first_name = raw_input("What's your first name please: ")
-    password = str(raw_input("Password please: "))
+    password = raw_input("Password please: ")
     lines = account.readlines()
 
     for index in range(len(lines)):
         dets = (lines[index].split(","))
 
         if (dets[0] == first_name) and (dets[2] == password):
-            print("First name and last name ", dets[0], dets[1])
-            print("Account Number: ", dets[4].strip())
-            print("Balance: ", dets[3].strip())
+            print "First name and last name: " + dets[0]+ " " + dets[1]
+            print "Account Number: " + dets[4].strip()
+            print "Balance: " + dets[3].strip()
             break
-        #else:
-        #    print("Error please check your name and password ")
+
 
 def change_password():
     account = open('data.txt', 'r+')
@@ -153,13 +143,14 @@ def change_password():
 
 
 while True:
-    choice = str(raw_input())
+
     print("\nPress 1 2 3 4 5 or type exit to quit:\n"
               "1.To create an account:\n"
               "2.To make a deposit: \n"
               "3.To withdraw money\n"
               "4.To Check the balance:\n"
               "5.Change password: \n")
+    choice = str(raw_input())
     try:
         if choice == '1':
             create_account()
@@ -174,7 +165,7 @@ while True:
         elif choice == 'exit':
             break
     except:
-        print("There is an error somewhere ")
+        print("Exception ")
 
 
 
